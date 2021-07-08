54268354d0acd266278d893068a4e37cb85a5889
Amir Haroun <60839505+AmirHaroun1@users.noreply.github.com>
2021-07-08 13:19:51 +0200

    Created migration for shipments

---
+++ database/migrations/2021_07_08_111001_create_shipments_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateShipmentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('shipments', function (Blueprint $table) {
+            $table->id();
+            $table->string('status');
+            $table->boolean('is_in_bound_shipment')->default(0);
+            $table->foreignId('customer_id');
+            $table->foreign('customer_id')
+                ->references('user_id')
+                ->on('customers')
+                ->onDelete('cascade');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('shipments');
+    }
+}
